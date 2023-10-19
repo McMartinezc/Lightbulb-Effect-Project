abd39de866b346467ccb61802f407514fd7e65de
McMartinezc <psyence_mc@hotmail.com>
2023-10-19 13:33:40 +0200

    fin

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 // Buena suerte ! Espero que se te encianda la "bombilla"!
+const bombilla = document.querySelector("#bulb");
+
+bombilla.addEventListener("click", ()=>{
+    bombilla.classList.toggle("bulb-on")
+})
