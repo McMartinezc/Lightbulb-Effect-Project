793fdc9b997798a42b81ae8955d268b4c2797036
McMartinezc <psyence_mc@hotmail.com>
2023-10-20 10:19:07 +0200

    fin

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 // Buena suerte ! Espero que se te encianda la "bombilla"!
 const bombilla = document.querySelector("#bulb");
 
-bombilla.addEventListener("click", ()=>{
-    bombilla.classList.toggle("bulb-on")
-})
+// bombilla.addEventListener("click", ()=>{
+//     bombilla.classList.toggle("bulb-on");
+// })
+
+bombilla.addEventListener("mouseover", () => {
+    bombilla.classList.add("bulb-on");
+    bombilla.classList.remove("bulb-off");
+});
+
+bombilla.addEventListener("mouseout", () => {
+    bombilla.classList.remove("bulb-on");
+    bombilla.classList.add("bulb-off");
+});
